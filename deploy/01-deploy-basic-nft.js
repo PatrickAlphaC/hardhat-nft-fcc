90944dfbbc3936f833e992e7ea4e332067322e97
PatrickAlphac <54278053+PatrickAlphaC@users.noreply.github.com>
2022-03-03 07:23:44 -0500

    init commit

---
+++ deploy/01-deploy-basic-nft.js
@@ -0,0 +1,29 @@
+const { network } = require("hardhat")
+const { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS } = require("../helper-hardhat-config")
+const { verify } = require("../helper-functions")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+    const chainId = network.config.chainId
+    const waitBlockConfirmations = developmentChains.includes(network.name)
+        ? 1
+        : VERIFICATION_BLOCK_CONFIRMATIONS
+
+    log("----------------------------------------------------")
+    arguments = []
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: arguments,
+        log: true,
+        waitConfirmations: waitBlockConfirmations,
+    })
+
+    // Verify the deployment
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, arguments)
+    }
+}
+
+module.exports.tags = ["all", "basicnft"]
---
+++ test/unit/randomIpfs.test.js
@@ -0,0 +1,44 @@
+// We are going to skimp a bit on these tests...
+
+const { assert, expect } = require("chai")
+const { network, deployments, ethers } = require("hardhat")
+const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+
+!developmentChains.includes(network.name)
+    ? describe.skip
+    : describe("Random IPFS NFT Unit Tests", async function () {
+          let randomIpfsNft, deployer, vrfCoordinatorV2Mock
+
+          beforeEach(async () => {
+              accounts = await ethers.getSigners()
+              deployer = accounts[0]
+              await deployments.fixture(["mocks", "randomipfs"])
+              randomIpfsNft = await ethers.getContract("RandomIpfsNft")
+              vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock")
+          })
+
+          describe("constructor", () => {
+              it("sets starting values correctly", async function () {
+                  const dogTokenUriZero = await randomIpfsNft.getDogTokenUris(0)
+                  const isInitialized = await randomIpfsNft.getInitialized()
+                  assert(dogTokenUriZero.includes("ipfs://"))
+                  assert.equal(isInitialized, true)
+              })
+          })
+
+          describe("requestNft", () => {
+              it("fails if payment isn't sent with the request", async function () {
+                  await expect(randomIpfsNft.requestNft()).to.be.revertedWith("NeedMoreETHSent")
+              })
+              it("emits and event and kicks off a random word request", async function () {
+                  const fee = await randomIpfsNft.getMintFee()
+                  await expect(randomIpfsNft.requestNft({ value: fee.toString() })).to.emit(
+                      randomIpfsNft,
+                      "NftRequested"
+                  )
+              })
+          })
+          describe("fulfillRandomWords", () => {
+              it("", async function () {})
+          })
+      })
