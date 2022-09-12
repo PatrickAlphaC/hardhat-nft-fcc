42d6e8201c7e014d2d530ef2b37e357db1c2017c
M.Siddique <108999811+ABossOfMyself@users.noreply.github.com>
2022-09-13 03:28:06 +0500

    Removed unused require from randomIpfs.test.js
    Removed unused require from helper-hardhat-config.

--- test/unit/randomIpfs.test.js
+++ test/unit/randomIpfs.test.js
@@ -2,7 +2,7 @@
 
 const { assert, expect } = require("chai")
 const { network, deployments, ethers } = require("hardhat")
-const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
+const { developmentChains } = require("../../helper-hardhat-config")
 
 !developmentChains.includes(network.name)
     ? describe.skip
