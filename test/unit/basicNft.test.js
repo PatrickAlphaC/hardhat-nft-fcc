de40ac2ad9da521f49f46fae072c6e141af08739
gabrieeel <gabrielp@gmail.com>
2022-10-03 20:35:26 -0300

    Add unit tests for balanceOf and ownerOf methods

--- test/unit/basicNft.test.js
+++ test/unit/basicNft.test.js
@@ -20,7 +20,7 @@ const { developmentChains } = require("../../helper-hardhat-config")
         
         //test01
         describe("Construtor", () => {
-            it("Initilizes the NFT Correctly.", async () => {
+            it("Initializes the NFT Correctly.", async () => {
                 const name = await basicNft.name()
                 const symbol = await basicNft.symbol()
                 const tokenCounter=await basicNft.getTokenCounter()
@@ -31,16 +31,24 @@ const { developmentChains } = require("../../helper-hardhat-config")
         })
 //test02
         describe("Mint NFT", () => {
-          it("Allows users to mint an NFT, and updates appropriately", async function () {
+          beforeEach(async () => {
               const txResponse = await basicNft.mintNft()
               await txResponse.wait(1)
+          })
+          it("Allows users to mint an NFT, and updates appropriately", async function () {
               const tokenURI = await basicNft.tokenURI(0)
               const tokenCounter = await basicNft.getTokenCounter()
 
               assert.equal(tokenCounter.toString(), "1")
               assert.equal(tokenURI, await basicNft.TOKEN_URI())
-             })
-         })
-        
-        
-      })
+          })
+          it("Show the correct balance and owner of an NFT", async function () {
+              const deployerAddress = deployer.address;
+              const deployerBalance = await basicNft.balanceOf(deployerAddress)
+              const owner = await basicNft.ownerOf("1")
+
+              assert.equal(deployerBalance.toString(), "1")
+              assert.equal(owner, deployerAddress)
+          })
+        })
+    })
