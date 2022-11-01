0c35e99dbb9369782157073cf13b5c1b22594865
Ryan Gannon <67101320+RyanPaulGannon@users.noreply.github.com>
2022-11-01 13:58:33 +0000

    Syntax update and removed unused import
    Updated to arrow function syntax and removed `ethers` from `hre` as it isn't being used.

--- deploy/01-deploy-basic-nft.ts
+++ deploy/01-deploy-basic-nft.ts
@@ -1,12 +1,12 @@
 import { developmentChains, VERIFICATION_BLOCK_CONFIRMATIONS} from "../helper-hardhat-config"
 import verify  from "../utils/verify"
-import {DeployFunction} from "hardhat-deploy/types"
-import {HardhatRuntimeEnvironment} from "hardhat/types"
+import { DeployFunction } from "hardhat-deploy/types"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
 
-const deployBasicNft: DeployFunction = async function (
+const deployBasicNft: DeployFunction = async (
     hre: HardhatRuntimeEnvironment
-  ) {
-    const { deployments, getNamedAccounts, network, ethers } = hre
+  ) => {
+    const { deployments, getNamedAccounts, network } = hre
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
     const waitBlockConfirmations = developmentChains.includes(network.name)
