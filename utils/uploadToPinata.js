a4118343501bbe4a7c7def2bf5461443f64f9bce
nagrarohit <121886772+nagrarohit@users.noreply.github.com>
2023-04-26 14:25:38 +0530

    Update uploadToPinata.js
    added require("dotenv").config()

--- utils/uploadToPinata.js
+++ utils/uploadToPinata.js
@@ -1,6 +1,7 @@
 const pinataSDK = require("@pinata/sdk")
 const fs = require("fs")
 const path = require("path")
+require("dotenv").config()
 
 const pinataApiKey = process.env.PINATA_API_KEY || ""
 const pinataApiSecret = process.env.PINATA_API_SECRET || ""
